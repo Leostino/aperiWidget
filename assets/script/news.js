de8cbc7b15b8e07a76f0bea54939351f33beba96
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-31 17:13:59 -0400

    adding the news js

--- assets/script/news.js
+++ assets/script/news.js
@@ -1,14 +1,14 @@
 const newsKey = "1215781e102941fc9d6c5bb9d5b6d7a4"
 
-let queryNewsKeyURL = "https://newsapi.org/v2/everything?q=bitcoin&from=2019-06-30&sortBy=publishedAt&apiKey=1215781e102941fc9d6c5bb9d5b6d7a4" + newsKey;
-
+let queryNewsKeyURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey="+newsKey;
+console.log(queryNewsKeyURL)
 $.ajax({
     url: queryNewsKeyURL,
     method:"GET"
 }).done(function(response){
     for (var i = 0;i<10;i++){
-        let newsevents = response.data[i].relationships.newsevents.data.id;
-        console.log("Routes"+response.data[i].relationships.newsevents.data);
-        $("#mbtaTest").append("<p>"+newsevents+"</p>");
+        let newsevents = response.[i];
+        console.log(response.articles[i]);
+        $("#news").append(response.articles[i]);
     }    
     })
