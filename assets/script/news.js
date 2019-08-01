b504e6dfd632d1992c74c91cf917081ec4fcb7bd
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-07-31 21:12:53 -0400

    got the news search bar to generate news

--- assets/script/news.js
+++ assets/script/news.js
@@ -1,14 +1,87 @@
-const newsKey = "1215781e102941fc9d6c5bb9d5b6d7a4"
-
-let queryNewsKeyURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey="+newsKey;
-console.log(queryNewsKeyURL)
-$.ajax({
-    url: queryNewsKeyURL,
-    method:"GET"
-}).done(function(response){
-    for (var i = 0;i<10;i++){
-        let newsevents = response.[i];
-        console.log(response.articles[i]);
-        $("#news").append(response.articles[i]);
-    }    
-    })
+$(document).ready(function(){
+    
+  $("#searchbtn").on("click",function(e){
+    e.preventDefault();
+    
+    let query = $("#searchquery").val();
+    let url = "https://newsapi.org/v2/top-headlines?q="+query+"&country=us&category=business&apiKey=750e76d88d5b4f5080959f48214d60d4";
+    
+    if(query !== ""){
+      
+      $.ajax({
+        
+        url: url,
+        method: "GET",
+        dataType: "json",
+        
+        beforeSend: function(){
+          $("#loader").show();
+        },
+        
+        complete: function(){
+          $("#loader").hide();
+        },
+        
+        success: function(news){
+          let output = "";
+          let latestNews = news.articles;
+          
+          for(var i in latestNews){
+            output +=`
+              <div class="col l6 m6 s12">
+              <h4>${latestNews[i].title}</h4>
+              <img src="${latestNews[i].urlToImage}" class="responsive-img">
+              <p>${latestNews[i].description}</p>
+              <p>${latestNews[i].content}</p>
+              <p>Published on: ${latestNews[i].publishedAt}</p>
+              <a href="${latestNews[i].url}" class="btn">Read more</a>
+              </div>
+            `;
+          }
+          
+          if(output !== ""){
+            $("#newsResults").html(output);
+             M.toast({
+              html: "There you go, nice reading",
+              classes: 'green'
+            });
+            
+          }else{
+            let noNews = `<div style='text-align:center; font-size:36px; margin-top:40px;'>This news isn't available. Sorry about that.<br>Try searching for something else </div>`;
+             $("#newsResults").html(noNews);
+            M.toast({
+              html: "This news isn't available",
+              classes: 'red'
+            });
+          }
+          
+        },
+        
+        error: function(){
+           let internetFailure = `
+           <div style="font-size:34px; text-align:center; margin-top:40px;">Please check your internet connection and try again.
+           <img src="img/internet.png" class="responsive-img">
+           </div>`;
+           
+          $("#newsResults").html(internetFailure);
+           M.toast({
+              html: "We encountered an error, please try again",
+              classes: 'red'
+            });
+        }
+        
+        
+      });
+      
+    }else{
+      let missingVal = `<div style="font-size:34px; text-align:center; margin-top:40px;">Please enter any search term in the search engine</div>`;
+      $("#newsResults").html(missingVal);
+       M.toast({
+              html: "Please enter something",
+              classes: 'red'
+            });
+    }
+    
+  });
+  
+});
