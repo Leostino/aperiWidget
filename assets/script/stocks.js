d6489b8e10ef78fade4768fe9a3bc7d464bef699
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-08-03 13:15:17 -0400

    adding stock chart

--- assets/script/stocks.js
+++ assets/script/stocks.js
@@ -1,9 +1,12 @@
 const iexToken = 'pk_5ac05ab4c58a4a39a2ebf4910d6a17ab';
 let symbol;
+let getLabels=[];
+let getSeries=[];
 
 
 let queryStocksURL;
 let queryLogoURL;
+let queryChartURL;
 getSymbol();
 
 function getSymbol(){
@@ -19,7 +22,7 @@ function callStock(){
 $.ajax({
     url: queryStocksURL,
     method:"GET"
-}).then(function(response){
+}).done(function(response){
     $("#companyName").html(response.companyName);
     $("#livePrice").html(response.latestPrice);
     $("#peRatio").html("P/E Ratio: "+response.peRatio);
@@ -33,8 +36,23 @@ $.ajax({
     url: queryLogoURL,
     method:"GET"
 }).done(function(response){
-    console.log(response);
     $("#logo").attr("src", response.url)
+
+});
+
+queryChartURL ="https://cloud.iexapis.com/v1/stock/"+symbol+"/chart/dynamic?token="+iexToken;   
+$.ajax({
+    url: queryChartURL,
+    method:"GET"
+}).done(function(response){
+    getLabels=[];
+    getSeries=[];
+
+    for (var i=0; i<response.data.length; i++){
+        getLabels.push(response.data[i].label);
+        getSeries.push(response.data[i].close);
+    }
+   plotChart(getLabels,getSeries);
 
 });
 }
@@ -63,3 +81,21 @@ function formatNumber(value) {
 }
 
 
+
+function plotChart(lab, ser){
+var data = {
+    // A labels array that can contain any sort of values
+    labels: lab,
+    // Our series array that contains series objects or in this case series data arrays
+    series:[ser]
+  };
+  
+  // As options we currently only set a static size of 300x200 px. We can also omit this and use aspect ratio containers
+  // as you saw in the previous example
+ 
+  
+  // Create a new line chart object where as first parameter we pass in a selector
+  // that is resolving to our chart container element. The Second parameter
+  // is the actual data object. As a third parameter we pass in our custom options.
+  new Chartist.Line('.ct-chart', data);
+}
